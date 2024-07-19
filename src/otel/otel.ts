53dc588defd3815066ea464c91abbe5f32a6c6ce
Jefferson Botitano Calderon Romero <jeffersonbotitano@gmail.com>
2024-07-18 21:48:28 -0300

    Adding metrics exporter.

--- src/otel/otel.ts
+++ src/otel/otel.ts
@@ -17,6 +17,11 @@ limitations under the License.
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
+import {
+  MeterProvider,
+  PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import opentelemetry, { Tracer } from "@opentelemetry/api";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
@@ -34,22 +39,23 @@ export class ElementCallOpenTelemetry {
   private _provider: WebTracerProvider;
   private _tracer: Tracer;
   private otlpExporter?: OTLPTraceExporter;
+  private metricProvider: MeterProvider;
+  private metricExporter: OTLPMetricExporter;
   public readonly rageshakeProcessor?: RageshakeSpanProcessor;
 
   public static globalInit(): void {
     const config = Config.get();
-    // we always enable opentelemetry in general. We only enable the OTLP
-    // collector if a URL is defined (and in future if another setting is defined)
-    // Posthog reporting is enabled or disabled
-    // within the posthog code.
-    const shouldEnableOtlp = Boolean(config.opentelemetry?.collector_url);
+    const shouldEnableOtlp = Boolean(
+      config.opentelemetry?.collector_url_traces,
+    );
 
     if (!sharedInstance || sharedInstance.isOtlpEnabled !== shouldEnableOtlp) {
       logger.info("(Re)starting OpenTelemetry debug reporting");
       sharedInstance?.dispose();
 
       sharedInstance = new ElementCallOpenTelemetry(
-        config.opentelemetry?.collector_url,
+        config.opentelemetry?.collector_url_traces,
+        config.opentelemetry?.collector_url_metrics,
         config.rageshake?.submit_url,
       );
     }
@@ -60,27 +66,67 @@ export class ElementCallOpenTelemetry {
   }
 
   private constructor(
-    collectorUrl: string | undefined,
+    traceCollectorUrl: string | undefined,
+    metricsCollectorUrl: string | undefined,
     rageshakeUrl: string | undefined,
   ) {
-    // This is how we can make Jaeger show a reasonable service in the dropdown on the left.
-    const providerConfig = {
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME,
-      }),
-    };
-    this._provider = new WebTracerProvider(providerConfig);
+    const resource = new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME,
+    });
 
-    if (collectorUrl) {
-      logger.info("Enabling OTLP collector with URL " + collectorUrl);
+    this.metricExporter = new OTLPMetricExporter({
+      url: metricsCollectorUrl,
+    });
+
+    // this.metricProvider = new MeterProvider({
+    //   resource,
+    // });
+
+    this.metricProvider = new MeterProvider({
+      resource,
+      readers: [
+        new PeriodicExportingMetricReader({
+          exporter: this.metricExporter,
+          exportIntervalMillis: 10000,
+        }),
+      ],
+    });
+
+    this._provider = new WebTracerProvider({ resource });
+
+    if (traceCollectorUrl) {
+      logger.info(
+        "Enabling OTLP traces collector with URL " + traceCollectorUrl,
+      );
       this.otlpExporter = new OTLPTraceExporter({
-        url: collectorUrl,
+        url: traceCollectorUrl,
       });
       this._provider.addSpanProcessor(
         new SimpleSpanProcessor(this.otlpExporter),
       );
     } else {
-      logger.info("OTLP collector disabled");
+      logger.info("OTLP traces collector disabled");
+    }
+
+    if (metricsCollectorUrl) {
+      logger.info(
+        "Enabling OTLP metrics collector with URL " + metricsCollectorUrl,
+      );
+      this.metricExporter = new OTLPMetricExporter({
+        url: metricsCollectorUrl,
+      });
+
+      this.metricProvider = new MeterProvider({
+        resource,
+        readers: [
+          new PeriodicExportingMetricReader({
+            exporter: this.metricExporter,
+            exportIntervalMillis: 10000,
+          }),
+        ],
+      });
+    } else {
+      logger.info("OTLP metrics collector disabled");
     }
 
     if (rageshakeUrl) {
@@ -90,16 +136,22 @@ export class ElementCallOpenTelemetry {
 
     this._provider.addSpanProcessor(new PosthogSpanProcessor());
     opentelemetry.trace.setGlobalTracerProvider(this._provider);
+    if (opentelemetry.metrics.setGlobalMeterProvider(this.metricProvider)) {
+      logger.info("Metrics provider set successfully");
+    }
 
-    this._tracer = opentelemetry.trace.getTracer(
-      // This is not the serviceName shown in jaeger
-      "my-element-call-otl-tracer",
-    );
+    this._tracer = opentelemetry.trace.getTracer("my-element-call-otl-tracer");
   }
 
   public dispose(): void {
     opentelemetry.trace.disable();
     this._provider?.shutdown();
+    this.metricProvider?.shutdown();
+  }
+
+  public forceFlush(): void {
+    this._provider.forceFlush();
+    this.metricProvider.forceFlush();
   }
 
   public get isOtlpEnabled(): boolean {
@@ -110,7 +162,7 @@ export class ElementCallOpenTelemetry {
     return this._tracer;
   }
 
-  public get provider(): WebTracerProvider {
-    return this._provider;
+  public get meterProvider(): MeterProvider {
+    return this.metricProvider;
   }
 }
